3a0f234ff1183de11159865039c1dabd009a677d
Sean Cali <seankenjicali@gmail.com>
2017-10-04 23:14:12 -0700

    Finished Module2 Lab2

---
+++ Module2/Mod2_Lab2/Mod2_Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates TechnicalEmployee Object with name Libby called employee1
+            var Employee1 = new TechnicalEmployee("Libyy");
+
+            // Instantiates TechnicalEmployee Object with name Zaynah called Employeed2
+            var Employee2 = new TechnicalEmployee("Zaynah");
+
+            // Instantiates BuisnessEmployee Object with name Wither called Employee3
+            var Employee3 = new BuisnessEmployee("Winter");
+
+            // Output to the console window
+            Console.WriteLine(Employee1.EmployeeStatus() + "..." + Employee2.EmployeeStatus() + "..." + Employee3.EmployeeStatus());
+        }
+    }
+}
---
+++ Module2/Mod2_Lab2/Mod2_Lab2/TechnicalEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab1
+{
+    // Inherit from Employee Class
+    class TechnicalEmployee : Employee
+    {
+        public TechnicalEmployee(String name) : base(name, 75000)
+        {
+
+        }
+
+        // Creates integer variable called "SuccessfulCheckIns" and assigns value to 5
+        public int SuccessfulCheckIns = 5;
+
+        // This method returns the toString() method - which is the employee's ID number and name - and prints the number of successful check ins the employee has had
+        public override string EmployeeStatus()
+        {
+            return this.toString() + " has " + this.SuccessfulCheckIns + " successful check ins";
+        }
+    }
+}
